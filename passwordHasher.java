69a68146c0fbe2228e2acf6003f8edfb35283868
Garrett Zary <gz1598@engr.ship.edu>
2023-04-18 10:41:48 -0400

    checks user and password of existing people

--- passwordHasher.java
+++ passwordHasher.java
@@ -2,16 +2,45 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class passwordHasher {
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, FileNotFoundException {
+    	File input = new File("data.txt");
+    	FileReader fr=new FileReader(input);   //reads the file  
+    	BufferedReader br=new BufferedReader(fr);  //creates a buffering character input stream  
+    	StringBuffer sb=new StringBuffer();    //constructs a string buffer with no characters  
+    	String line = null;  
+    	Boolean userExist = false;
+    	Boolean passwordCorrect = false;
+    	
     	Scanner in = new Scanner(System.in);
     	
     	//Get user name input from use
     	System.out.print("Enter your username: ");
         String user = in.nextLine();
+        
+     // Try block to check if exception occurs
+        try {
+        	while(((line=br.readLine())!=null) && (userExist == false))  
+        	{  
+        		userExist = line.contains(user);
+        		sb.append(line);      //appends line to string buffer  
+        		sb.append("\n");     //line feed   
+        	} 
+        	fr.close();
+        	br.close();
+        }
+        // Catch block to handle the exception
+        catch (IOException e) {
+            System.out.print(e.getMessage());
+        }
         
         // Get password input from user
         System.out.print("Enter your password: ");
@@ -33,24 +62,36 @@ public class passwordHasher {
             hexString.append(hex);
         }
 
-        String output = ("Username: " + user + '\n' + "Hashed password: " + hexString.toString());
+        String output = ("Username: " + user + '\t' + "Hashed password: " + hexString.toString() + '\n');
         // Print the username and hashed password in hexadecimal format
         System.out.println(output);
        
-        FileOutputStream outputStream = null;
-        
-        // Try block to check if exception occurs
-        try {
-            outputStream = new FileOutputStream("data.txt");
-            // Store byte content from string
-            byte[] strToBytes = output.getBytes();
-
-            // Write into the file
-            outputStream.write(strToBytes);
-        }
-        // Catch block to handle the exception
-        catch (IOException e) {
-            System.out.print(e.getMessage());
+        if (userExist == false) {
+	        FileOutputStream outputStream = null;
+	        
+	        // Try block to check if exception occurs
+	        try {
+	            outputStream = new FileOutputStream("data.txt", true);
+	            // Store byte content from string
+	            byte[] strToBytes = output.getBytes();
+	
+	            // Write into the file
+	            outputStream.write(strToBytes);
+	            
+	            outputStream.close();
+	        }
+	        // Catch block to handle the exception
+	        catch (IOException e) {
+	            System.out.print(e.getMessage());
+	        }
+        } else {
+        	passwordCorrect = line.contains(hexString.toString());
+        	
+        	if (passwordCorrect == true) {
+        		System.out.println("Welcome back " + user);
+        	} else {
+        		System.out.println("The password is incorrect, please try again");
+        	}
         }
     }
 }
